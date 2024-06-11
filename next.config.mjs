081fdf883856414f0e38711b7d7b5ad35c699250
Mr.Chu1ka <CAC228@users.noreply.github.com>
2024-06-11 18:37:59 +0300

    deploooy

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 import next from 'next';
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          '@': path.resolve(__dirname, './'),
-        };
-    
-        return config;
-      },
     reactStrictMode: true,
     
 };
