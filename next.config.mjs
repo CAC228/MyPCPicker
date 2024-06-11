8a1427c78859ac18e5a4b6a0c76a084ebe017987
Mr.Chu1ka <CAC228@users.noreply.github.com>
2024-06-11 18:24:52 +0300

    true

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@ import next from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          '@': path.resolve(__dirname, './'),
+        };
+    
+        return config;
+      },
     reactStrictMode: true,
+    
 };
 
 export default nextConfig;
